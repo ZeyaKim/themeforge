a319d9c3fbce6f7382da65835c8d857c23dad301
ZeyaKim <chlwltjrdev@gmail.com>
2024-02-15 04:55:04 +0900

    🎨 refactor thememanager

--- src/theme_manager.js
+++ src/theme_manager.js
@@ -1,19 +1,19 @@
 import Theme from "./theme.js";
 
 const ThemeManager = (function() {
+  /*
   const currentTheme = new Theme({
     primary_color: "red",
     secondary_color: "green",
     background_color: "blue",
     font_color: "white",
   })
+  */
+  let exampleThemes = [];
+  let savedThemes = [];
 
   return {
-    extractThemeDataFromJson: function(jsonData) {
-
-    },
     createTheme: function(keyword, themeDataJson) {
-
     },
     deleteTheme: function(index) {
 
